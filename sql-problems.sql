04e82b578e819eea4ec2a086ed8f09204a60eff5
Maria <51481457+MariaLavrovskaya@users.noreply.github.com>
2021-10-14 10:19:33 +0100

    Create sql-problems.sql
    SQL problems from Leetcode

---
+++ sql-problems.sql
@@ -0,0 +1,8 @@
+-- Write an SQL query to report all the duplicate emails. https://leetcode.com/problems/duplicate-emails/
+SELECT Email FROM(SELECT Email from Person group by Email having count(*)>1) Person2;
+
+-- Please write a sql query to get the amount of each follower’s follower if he/she has one. https://leetcode.com/problems/second-degree-follower/
+SELECT followee as follower, count(follower) as num from (SELECT distinct * FROM follow) A
+WHERE followee IN (select follower from follow) 
+GROUP BY followee
+ORDER BY follower ASC
