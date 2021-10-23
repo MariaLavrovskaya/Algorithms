15c2ed21bdc0b86d113ab96739387352a51448e9
Maria <51481457+MariaLavrovskaya@users.noreply.github.com>
2021-10-23 15:20:48 +0100

    Update reverse-linked-list.py

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -1,5 +1,10 @@
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution:
     def __init__(self):
