c927bc692538c5831319153575e6c97f4d160dc5
Maria <51481457+MariaLavrovskaya@users.noreply.github.com>
2021-11-03 18:18:35 +0000

    Update merge-two-linked-lists.py

--- merge-two-linked-lists.py
+++ merge-two-linked-lists.py
@@ -5,6 +5,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         first_list_iterator = l1
